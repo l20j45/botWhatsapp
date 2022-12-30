e100e935a11a9a278f92d266c9b2edd72e5950b1
Daniel Rojas <l20j45_@gmail.com>
2022-12-30 12:29:46 -0600

    finish this repo

---
+++ config/mysql.js
@@ -0,0 +1,19 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : process.env.SQL_HOST || 'localhost',
+  user     : process.env.SQL_USER || 'root',
+  port     : process.env.SQL_PORT || '3306',
+  password : process.env.SQL_PASS || '',
+  database : process.env.SQL_DATABASE || 'pruebas'
+});
+
+const connect = () => connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Conexion correcta con tu base de datos MySQL')
+});
+
+module.exports = {connect, connection}
